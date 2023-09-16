d42a40b23850dd241f28e22d5c8eb1b4cc60cc6c
sayan <sayankarmakar09990@gmail.com>
2023-09-15 20:55:56 -0400

    initial commit

---
+++ src/main/java/com/sayan/TrainTix/TrainTixApplication.java
@@ -0,0 +1,13 @@
+package com.sayan.TrainTix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainTixApplication.class, args);
+	}
+
+}
